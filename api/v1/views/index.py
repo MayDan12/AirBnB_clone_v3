1e17e76e08683768cdad13555818119022e7f5dd
Adebayo Precious <ifeoluwa.adebayo2003@gmail.com>
2024-02-26 01:27:06 +0000

    API stat

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -11,3 +11,29 @@ def status():
     Description: Returns a JSON representation of the status of the API.
     """
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    """
+    Route: /stats
+    Method: GET
+    Description: Returns a JSON representation of the
+    number of each object type in the storage.
+    """
+    from models import storage
+    from models.state import State
+    from models.city import City
+    from models.amenity import Amenity
+    from models.place import Place
+    from models.review import Review
+    from models.user import User
+
+    return jsonify({
+        "states": storage.count(State),
+        "cities": storage.count(City),
+        "amenities": storage.count(Amenity),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "users": storage.count(User)
+    })
